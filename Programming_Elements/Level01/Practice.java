b7a9d3a483eeab4740c98417ffd73f6846fd15c0
Vivrz <viveksingh1_cs22@gla.ac.in>
2025-06-28 09:30:27 +0530

    [Vivek] , Workshop completed

--- Programming_Elements/Level01/Practice.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Practice {
-	public static void main(String args []) {
-		Scanner sc = new Scanner(System.in);
-		for(int i = 1 ;i <= 10 ;i++ ) {
-			System.out.println("2 * " + i + " = " + (2*i));
-		}
-	}
-}
